4572d9b43e4a5a4daeb0dd089cd6d06de4b71270
Course Test <sungwi@cs.sfu.ca>
2024-01-18 02:05:35 -0800

    add pages

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 
     if(document.body.classList.contains('dark-mode')){
-        document.getElementById('darkIcon').src = 'icons/efy.png';
+        document.getElementById('darkmode').src = '../icons/fukidashi_light_01.png';
+        document.getElementById('darkIcon').src = '../icons/efy.png';
     }else{
-        document.getElementById('darkIcon').src = 'icons/burakky.png';
+        document.getElementById('darkmode').src = '../icons/fukidashi_maru_02.png';
+        document.getElementById('darkIcon').src = '../icons/burakky.png';
     }
 });
 
