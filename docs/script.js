0be587f29f340baf39849cd4b6ff1cdc1b7eae92
Course Test <sungwi@cs.sfu.ca>
2024-01-28 22:48:43 -0800

    update mypage

--- docs/script.js
+++ docs/script.js
@@ -2,11 +2,12 @@ document.getElementById('darkModeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 
     if(document.body.classList.contains('dark-mode')){
-        document.getElementById('darkmode').src = "https://github.com/sungwi/MyPage/blob/main/icons/fukidashi_light_01.png?raw=true";
-        document.getElementById('darkIcon').src = "https://github.com/sungwi/MyPage/blob/main/icons/efy.png?raw=true";
+       // document.getElementById('darkmode').src = "https://github.com/sungwi/MyPage/blob/main/icons/fukidashi_light_01.png?raw=true";
+        document.getElementById('darkIcon').src = "../icons/sun.png";
+        document.getElementById('darkIcon').style.background = 'white';
     }else{
-        document.getElementById('darkmode').src = "https://github.com/sungwi/MyPage/blob/main/icons/fukidashi_maru_02.png?raw=true";
-        document.getElementById('darkIcon').src = "https://github.com/sungwi/MyPage/blob/main/icons/burakky.png?raw=true";
+       // document.getElementById('darkmode').src = "https://github.com/sungwi/MyPage/blob/main/icons/fukidashi_maru_02.png?raw=true";
+        document.getElementById('darkIcon').src = "../icons/moon.png";
     }
 });
 
